0255c6ab672ef385d2363beeb0c05d73b890a593
misbah999u <155499673+misbah999u@users.noreply.github.com>
2026-03-30 11:14:01 -0500

    Delete Regular-Meal-CMS/Regular-Meal-CMS directory

--- Regular-Meal-CMS/Regular-Meal-CMS/delivery/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from delivery.models import DeliveryVendor
-
-class DeliveryVendorSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = DeliveryVendor
-        fields = ['title', 'description', 'price_one_delivery']
--- Regular-Meal-CMS/Regular-Meal-CMS/delivery/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from delivery.views import DeliveryVendorListView
-
-urlpatterns = [
-    path('<str:language>/delivery/', DeliveryVendorListView.as_view(), name='delivery-list'),
-
-]
